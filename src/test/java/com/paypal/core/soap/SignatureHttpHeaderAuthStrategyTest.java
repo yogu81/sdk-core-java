72fcca1a93ce110588cc4d99fbdcbcbea2469a2a
tarini <tarininegi@gmail.com>
2012-10-23 11:42:51 +0530

    java test cases for  'com.paypal.core.soap' committed

--- src/test/java/com/paypal/core/soap/SignatureHttpHeaderAuthStrategyTest.java
+++ src/test/java/com/paypal/core/soap/SignatureHttpHeaderAuthStrategyTest.java
@@ -6,6 +6,7 @@ import junit.framework.Assert;
 
 import org.testng.annotations.Test;
 
+import com.paypal.core.credential.CertificateCredential;
 import com.paypal.core.credential.SignatureCredential;
 import com.paypal.core.credential.TokenAuthorization;
 
@@ -21,5 +22,18 @@ public class SignatureHttpHeaderAuthStrategyTest {
 		String[] headers=authHeader.split(",");
 		
 		Assert.assertEquals("token=accessToken", headers[0]);
+	}
+	
+	@Test
+	public void generateHeaderStrategyWithoutTokenTest() throws Exception{
+		SignatureHttpHeaderAuthStrategy signatureHttpHeaderAuthStrategy= new SignatureHttpHeaderAuthStrategy("https://svcs.sandbox.paypal.com/");
+		SignatureCredential signatureCredential = new SignatureCredential("testusername","testpassword","testsignature");
+		Map<String,String> header = signatureHttpHeaderAuthStrategy.generateHeaderStrategy(signatureCredential);
+		String username = (String)header.get("X-PAYPAL-SECURITY-USERID");
+		String psw = (String)header.get("X-PAYPAL-SECURITY-PASSWORD");
+		String sign = (String)header.get("X-PAYPAL-SECURITY-SIGNATURE");
+		Assert.assertEquals("testusername", username);
+		Assert.assertEquals("testpassword", psw);
+		Assert.assertEquals("testsignature", sign);
 	}
 }
