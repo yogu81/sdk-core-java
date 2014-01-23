5a170d0c304c069196b4690a2d108dea756c992d
Avi Das <adas@colgate.edu>
2014-01-23 11:58:51 -0800

    fix to remove unused variable random

--- src/test/java/com/paypal/core/ConfigManagerTest.java
+++ src/test/java/com/paypal/core/ConfigManagerTest.java
@@ -19,19 +19,12 @@ public class ConfigManagerTest {
 	}
 
 	@Test(dataProvider = "configParams", dataProviderClass = DataProviderClass.class)
-	public void getValueTest(ConfigManager conf) {
-		Assert.assertEquals(conf.getConfigurationMap().get("service.EndPoint"),
-				UnitTestConstants.API_ENDPOINT);
-	}
-
-	@Test(dataProvider = "configParams", dataProviderClass = DataProviderClass.class)
 	public void getValuesByCategoryTest(ConfigManager conf) {
 		Map<String, String> map = conf.getValuesByCategory("acct");
 		Iterator<String> itr = map.keySet().iterator();
 		while (itr.hasNext()) {
 			assert (((String) itr.next()).contains("acct"));
 		}
-
 	}
 
 	@Test(dataProvider = "configParams", dataProviderClass = DataProviderClass.class)
